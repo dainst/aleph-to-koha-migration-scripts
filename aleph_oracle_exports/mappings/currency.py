34270aebddcaee8f2a0fe30b6d1ac02acedaec3c
Simon Hohl <simon.hohl@dainst.org>
2018-01-12 13:48:41 +0100

    Make currency mapping more strict
    If an unknown currency is encountered, the script now prints a warning
    and returns `None`.

--- aleph_oracle_exports/mappings/currency.py
+++ aleph_oracle_exports/mappings/currency.py
@@ -5,15 +5,33 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 # Siehe http://confluence:8090/pages/viewpage.action?pageId=44859465
-# bzw. docker
+# bzw. Docker
 
 CURRENCY_MAPPING = {
-    'ILS': 'NIS'
+    'AUD': 'AUD',
+    'BGN': 'BGN',
+    'CAD': 'CAD',
+    'CHF': 'CHF',
+    'CNY': 'CNY',
+    'DKK': 'DKK',
+    'EGP': 'EGP',
+    'EUR': 'EUR',
+    'GBP': 'GBP',
+    'ILS': 'NIS',
+    'JPY': 'JPY',
+    'NOK': 'NOK',
+    'RUB': 'RUB',
+    'SEK': 'SEK',
+    'TRY': 'TRY',
+    'USD': 'USD'
 }
 
 
 def map_from_currency(aleph_currency):
-    if aleph_currency not in CURRENCY_MAPPING:
-        return aleph_currency
+    if aleph_currency is None:
+        return None
+    elif aleph_currency not in CURRENCY_MAPPING:
+        logger.warning("The currency code " + aleph_currency + " could not be mapped.")
+        return None
     else:
         return CURRENCY_MAPPING[aleph_currency]
