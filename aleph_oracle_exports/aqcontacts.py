b8e285ede4eb84afb45955299e14c8f0c1bcaa23
Simon Hohl <simon.hohl@dainst.org>
2018-01-16 18:04:48 +0100

    Add oracle export script for aqcontacts data
    Closes Redmine tickets #7487 and #7488.

---
+++ aleph_oracle_exports/aqcontacts.py
@@ -0,0 +1,218 @@
+import logging
+import sys
+
+import database_connections.mariadb as mariadb
+import database_connections.oracle as oracle
+import oracle_helper.z70 as z70_helper
+import oracle_helper.z72 as z72_helper
+
+logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+MAPPING_SQL_OUTPUT_PATH = './aleph_oracle_exports/mariadb_intermediate_values/00200_aqcontacts_data_mapping.sql'
+IMPORT_SQL_OUTPUT_PATH = './aleph_oracle_exports/ready_for_import/aqcontacts_data_import.sql'
+
+
+def map_boolean_to_0_or_1(boolean_value):
+    if boolean_value:
+        return 1
+    else:
+        return 0
+
+
+def create_name_from_type(address_type):
+    if address_type == 1:
+        return 'Vendor contact'
+    elif address_type == 2:
+        return 'Claim contact'
+    elif address_type == 3:
+        return 'Claim contact'
+    elif address_type == 4:
+        return 'Claim contact'
+    else:
+        None
+
+
+def process_z72_result(previous_results, query_result):
+    updated_results = previous_results.copy()
+
+    [vendor_key, address_type] = z72_helper.split_rec_key(query_result[0])
+    if int(address_type) < 1 or int(address_type) > 4:
+        return previous_results
+
+    aqbookseller_data = mariadb.get_aqbookseller_by_aleph_key(vendor_key)
+
+    aqcontacts_data = {
+        'phone': query_result[3],
+        'email': query_result[5],
+        'notes': query_result[11],
+    }
+
+    z70_data = oracle.get_z70_by_rec_key(vendor_key.ljust(25))
+
+    if z70_data.rowcount > 1:
+        logger.error('Found more than one dataset in Z70 for vendor key ' + vendor_key)
+
+    parsed_z70_data = z70_helper.parse_contact_fields(z70_data)
+
+    if 'name' in parsed_z70_data:
+        aqcontacts_data['name'] = parsed_z70_data['name']
+
+    if 'email' in parsed_z70_data and aqcontacts_data['email'] is None:
+        aqcontacts_data['email'] = parsed_z70_data['email']
+
+    if 'email' in parsed_z70_data and aqcontacts_data['email'] is not None:
+
+        if parsed_z70_data['email'] != aqcontacts_data['email'].strip():
+            logger.warning('Email provided in both z70 and z72, keeping value from z72.')
+            logger.warning('z70: ' + parsed_z70_data['email'])
+            logger.warning('z72: ' + aqcontacts_data['email'])
+
+    if all(value is None for value in aqcontacts_data.values()):
+        return previous_results
+
+    if 'name' not in aqcontacts_data:
+        aqcontacts_data['name'] = create_name_from_type(int(address_type))
+
+    aqcontacts_data['orderacquisition'] = map_boolean_to_0_or_1(int(address_type) == 1)
+    aqcontacts_data['claimacquisition'] = map_boolean_to_0_or_1(int(address_type) == 2)
+    aqcontacts_data['claimissues'] = map_boolean_to_0_or_1(int(address_type) == 2)
+    aqcontacts_data['acqprimary'] = 0
+    aqcontacts_data['serialsprimary'] = 0
+    aqcontacts_data['booksellerid'] = aqbookseller_data[0]
+
+    if vendor_key not in previous_results:
+        updated_results[vendor_key] = []
+
+    updated_results[vendor_key].append(aqcontacts_data)
+
+    return updated_results
+
+
+def fetch_data(credentials):
+    oracle.establish_connection(credentials)
+    mariadb.establish_connection()
+
+    z72_result = dict()
+    cur = oracle.get_z72()
+    for query_result in cur:
+        z72_result = process_z72_result(z72_result, query_result)
+
+    cur.close()
+    oracle.close_connection()
+
+    return z72_result
+
+
+def generate_insert_statement(aleph_key, data, produce_mapping_table):
+    statement = 'INSERT INTO aqcontacts ('
+    keys = data.keys()
+    keys_len = len(data)
+    for idx, key in enumerate(keys):
+
+        if data[key] is None:
+            continue
+
+        if idx == keys_len - 1:
+            statement += key
+
+            if produce_mapping_table:
+                statement += ', ALEPH_VENDOR_KEY'
+        else:
+            statement += key + ','
+
+    statement += ') VALUES('
+
+    for idx, key in enumerate(keys):
+
+        if data[key] is None:
+            continue
+
+        if idx == keys_len - 1:
+            statement += '"' + str(data[key]) + '"'
+            if produce_mapping_table:
+                statement += ', "' + str(aleph_key) + '"'
+        else:
+            statement += '"' + str(data[key]) + '",'
+
+    statement += ')'
+    statement += ';\n'
+
+    return statement
+
+
+def filter_duplicates(results):
+
+    updated_results = {}
+
+    for vendor_key in results:
+
+        contact_list = sorted(results[vendor_key], key=lambda k: k['name'])
+
+        updated_contact_list = []
+
+        index = 0
+
+        while index < len(contact_list):
+            if len(updated_contact_list) == 0 or updated_contact_list[-1]['name'] != contact_list[index]['name']:
+                updated_contact_list.append(contact_list[index])
+            else:
+                for key in contact_list[index]:
+                    value = contact_list[index][key]
+                    if (key not in updated_contact_list[-1]
+                            or updated_contact_list[-1][key] is None
+                            or updated_contact_list[-1][key] == 0):
+                        updated_contact_list[-1][key] = value
+                    if (updated_contact_list[-1][key] is not None
+                            and value is not None
+                            and not (value == 0 and updated_contact_list[-1][key] == 1)
+                            and updated_contact_list[-1][key] != value):
+                        logger.warning('Unhandled case: 2 contacts with same name have both a different value set for')
+                        logger.warning(' name: ' + updated_contact_list[-1]['name'])
+                        logger.warning(' key: ' + str(key))
+                        logger.warning(' first contact value: ' + str(value))
+                        logger.warning(' second contact value: ' + str(updated_contact_list[-1][key]))
+                        logger.warning('Second variant will be skipped.')
+
+            index += 1
+
+        updated_results[vendor_key] = updated_contact_list
+    return updated_results
+
+
+def write_data(data):
+    logger.info('Writing data to file and mapping database.')
+
+    mariadb.establish_connection()
+
+    cursor = mariadb.get_cursor()
+    with open(IMPORT_SQL_OUTPUT_PATH, 'w') as import_file, open(MAPPING_SQL_OUTPUT_PATH, 'w') as mapping_file:
+
+        mapping_file.write('USE ' + mariadb.get_db_name() + ';')
+
+        for aleph_key in data.keys():
+
+            for contact in data[aleph_key]:
+
+                import_file.write(generate_insert_statement(aleph_key, contact, False))
+
+                mapping_statement = generate_insert_statement(aleph_key, contact, True)
+
+                mapping_file.write(mapping_statement)
+                cursor.execute(mapping_statement)
+
+    mariadb.commit()
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        logger.info('Please provide as argument:')
+        logger.info('1) Connection info and credentials, pattern: "%USER%/%PASSWORD%@%IP%/%SID%".')
+        sys.exit()
+
+    results = fetch_data(sys.argv[1])
+    filtered_results = filter_duplicates(results)
+    write_data(filtered_results)
+
--- aleph_oracle_exports/oracle_helper/z70.py
+++ aleph_oracle_exports/oracle_helper/z70.py
@@ -1,2 +1,46 @@
+import logging
+
+logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
 def split_rec_key(rec_key):
     return [rec_key[0:-5].strip(), rec_key[-5].strip()]
+
+
+def parse_contact_fields(z70_data):
+
+    result = {}
+
+    for z70_query_result in z70_data:
+        # Skip ROM for now.
+        if z70_query_result[0].startswith('R/'):
+            return result
+        else:
+            z70_contact_fields = z70_query_result[8:12]
+
+            if all(value is None for value in z70_contact_fields):
+                continue
+            else:
+                for value in z70_contact_fields:
+                    if value is None:
+                        continue
+                    elif ('IBAN' in value) or ('BIC' in value) or ('BLZ' in value):
+                        result['bank_account'] = value.strip()
+                    elif '@' in value:
+                        result['email'] = value.strip()
+                    elif len(value.strip()) > 40:
+                        logger.debug('Unable to decide what this is, string is too long, skipping:')
+                        logger.debug(value)
+                        logger.debug('VENDOR_KEY: ' + z70_query_result[0])
+                        continue
+                    elif len(value) < 4:
+                        logger.debug('Unable to decide what this is, string is too short, skipping:')
+                        logger.debug(value)
+                        logger.debug('VENDOR_KEY: ' + z70_query_result[0])
+                        continue
+                    else:
+                        result['name'] = value.strip()
+
+    return result
