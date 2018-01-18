53ea1bba1e05953e49c6bc130f07addc3b21cb31
Simon Hohl <simon.hohl@dainst.org>
2018-01-18 15:41:50 +0100

    Remove special 'if' branch for ROM

--- aleph_oracle_exports/oracle_helper/z70.py
+++ aleph_oracle_exports/oracle_helper/z70.py
@@ -18,53 +18,49 @@ def parse_contact_fields(z70_data):
     result = {}
 
     for z70_query_result in z70_data:
-        # Skip ROM for now.
-        if z70_query_result[0].startswith('R/'):
-            return result
+        z70_contact_fields = z70_query_result[8:12]
+
+        if all(value is None for value in z70_contact_fields):
+            continue
         else:
-            z70_contact_fields = z70_query_result[8:12]
+            for value in z70_contact_fields:
+                if value is None:
+                    continue
+                elif ('IBAN' in value) or ('BIC' in value) or ('BLZ' in value):
+                    result['bank_account'] = value.strip()
+                elif '@' in value:
+                    match = re.search(EXTRACT_EMAIL_REGEX, value)
+                    if match:
+                        result['email'] = match.group(1)
 
-            if all(value is None for value in z70_contact_fields):
-                continue
-            else:
-                for value in z70_contact_fields:
-                    if value is None:
-                        continue
-                    elif ('IBAN' in value) or ('BIC' in value) or ('BLZ' in value):
-                        result['bank_account'] = value.strip()
-                    elif '@' in value:
-                        match = re.search(EXTRACT_EMAIL_REGEX, value)
-                        if match:
-                            result['email'] = match.group(1)
+                        if match.group(0) != match.group(1):
+                            logger.warning('Value contains data besides email adress:' + match.group(1))
+                            logger.warning(' ' + value)
+                            logger.warning(' Writing rest in "notes".')
 
-                            if match.group(0) != match.group(1):
-                                logger.warning('Value contains data besides email adress:' + match.group(1))
-                                logger.warning(' ' + value)
-                                logger.warning(' Writing rest in "notes".')
+                            rest = match.group(0).replace(match.group(1), '')
+                            if 'notes' not in result:
+                                result['notes'] = ''
+                            result['notes'] += ', ' + rest
+                elif len(value.strip()) > 20:
+                    logger.warning('Unable to decide what this is, string is too long:')
+                    logger.warning(value)
+                    logger.warning(' VENDOR_KEY: ' + z70_query_result[0])
+                    logger.warning(' Writing to "notes".')
 
-                                rest = match.group(0).replace(match.group(1), '')
-                                if 'notes' not in result:
-                                    result['notes'] = ''
-                                result['notes'] += ', ' + rest
-                    elif len(value.strip()) > 20:
-                        logger.warning('Unable to decide what this is, string is too long:')
-                        logger.warning(value)
-                        logger.warning(' VENDOR_KEY: ' + z70_query_result[0])
-                        logger.warning(' Writing to "notes".')
+                    if 'notes' not in result:
+                        result['notes'] = ''
+                    result['notes'] += ', ' + value
+                elif len(value) < 4:
+                    logger.warning('Unable to decide what this is, string is too short:')
+                    logger.warning(value)
+                    logger.warning(' VENDOR_KEY: ' + z70_query_result[0])
+                    logger.warning(' Writing to "notes".')
 
-                        if 'notes' not in result:
-                            result['notes'] = ''
-                        result['notes'] += ', ' + value
-                    elif len(value) < 4:
-                        logger.warning('Unable to decide what this is, string is too short:')
-                        logger.warning(value)
-                        logger.warning(' VENDOR_KEY: ' + z70_query_result[0])
-                        logger.warning(' Writing to "notes".')
-
-                        if 'notes' not in result:
-                            result['notes'] = ''
-                        result['notes'] += ', ' + value
-                    else:
-                        result['name'] = value.strip()
+                    if 'notes' not in result:
+                        result['notes'] = ''
+                    result['notes'] += ', ' + value
+                else:
+                    result['name'] = value.strip()
 
     return result
