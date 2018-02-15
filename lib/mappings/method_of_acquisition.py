387e179498da67bd74417de644218eee343f894a
Simon Hohl <simon.hohl@dainst.org>
2018-02-15 15:42:02 +0100

    Add mapping for Aleph's keys for acquisition methods

---
+++ lib/mappings/method_of_acquisition.py
@@ -0,0 +1,22 @@
+import logging
+
+logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+METHODS = {
+    'A': 'zur Ansicht',
+    'P': 'Kauf',
+    'PF': 'Kauf (frei)',
+    'G': 'Geschenk',
+    'E': 'Tausch',
+    'D': 'Pflicht',
+    'O': 'Andere'
+}
+
+
+def map_aleph_key(aleph_key):
+    if aleph_key not in METHODS:
+        logger.warning("Unknown method of acquisition key: " + aleph_key + ", defaulting to " + METHODS['O'])
+        return METHODS['O']
+    return METHODS[aleph_key]
