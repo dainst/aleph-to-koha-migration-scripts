8318e759e43bbe67dd81efc4f00b008d3203f926
Simon Hohl <simon.hohl@dainst.org>
2018-03-09 15:43:30 +0100

    Setup mapping table aqorders_items

---
+++ lib/mariadb_table_setup/aqorders_items_create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `aqorders_items` (
+  `ordernumber` int(11) NOT NULL,
+  `itemnumber` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`itemnumber`),
+  KEY `ordernumber` (`ordernumber`),
+  CONSTRAINT `aqorders_items_ibfk_1` FOREIGN KEY (`ordernumber`) REFERENCES `aqorders` (`ordernumber`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
