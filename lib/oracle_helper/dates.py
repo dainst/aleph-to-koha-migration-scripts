6fb0078cd434fe47468be5afdc3273632b7c235f
Simon Hohl <simon.hohl@dainst.org>
2018-03-27 13:53:16 +0200

    Handle Aleph dates provided as digits

--- lib/oracle_helper/dates.py
+++ lib/oracle_helper/dates.py
@@ -12,10 +12,22 @@ def process_aleph_date(aleph_date):
     :return: koha_date: YYYY-MM-DD
     """
 
-    if aleph_date == '00000000' or aleph_date == 0 or aleph_date is None:
+    if aleph_date is None:
         return None
-    elif len(aleph_date) != 8:
-        logger.error('Aleph date format error: %s', aleph_date)
-        return None
+
+    if type(aleph_date) == str:
+        if aleph_date == '00000000':
+            return None
+        elif len(aleph_date) != 8:
+            logger.error('Aleph date format error: %s', aleph_date)
+            return None
+        else:
+            return "-".join([aleph_date[0:4], aleph_date[4:6], aleph_date[6:8]])
+    elif type(aleph_date) == int:
+        if aleph_date == 0:
+            return None
+        else:
+            date_as_string = str(aleph_date)
+            return "-".join([date_as_string[0:4], date_as_string[4:6], date_as_string[6:8]])
     else:
-        return "-".join([aleph_date[0:4], aleph_date[4:6], aleph_date[6:8]])
+        logger.error('Aleph date format error: %s', str(aleph_date))
