5e78838a66fc9577e384ab0f1bebb0b58dcefef8
Simon Hohl <simon.hohl@dainst.org>
2018-04-10 10:52:18 +0200

    Do not add default data for aqbudgets and aqbudgetperiods

---
+++ lib/mariadb_table_setup/aqbudgetperiods_create.sql
@@ -0,0 +1,15 @@
+USE koha_mapping_db;
+
+CREATE TABLE `aqbudgetperiods` (
+  `budget_period_id` int(11) NOT NULL AUTO_INCREMENT,
+  `budget_period_startdate` date NOT NULL,
+  `budget_period_enddate` date NOT NULL,
+  `budget_period_active` tinyint(1) DEFAULT 0,
+  `budget_period_description` mediumtext COLLATE utf8_unicode_ci DEFAULT NULL,
+  `budget_period_total` decimal(28,6) DEFAULT NULL,
+  `budget_period_locked` tinyint(1) DEFAULT NULL,
+  `sort1_authcat` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sort2_authcat` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ALEPH_Z76_BUDGET_NUMBER` varchar(25) NOT NULL,
+  PRIMARY KEY (`budget_period_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
---
+++ lib/mariadb_table_setup/aqbudgets_create.sql
@@ -0,0 +1,25 @@
+USE koha_mapping_db;
+
+CREATE TABLE `aqbudgets` (
+  `budget_id` int(11) NOT NULL AUTO_INCREMENT,
+  `budget_parent_id` int(11) DEFAULT NULL,
+  `budget_code` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `budget_name` varchar(80) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `budget_branchcode` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `budget_amount` decimal(28,6) DEFAULT 0.000000,
+  `budget_encumb` decimal(28,6) DEFAULT 0.000000,
+  `budget_expend` decimal(28,6) DEFAULT 0.000000,
+  `budget_notes` mediumtext COLLATE utf8_unicode_ci DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `budget_period_id` int(11) DEFAULT NULL,
+  `sort1_authcat` varchar(80) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sort2_authcat` varchar(80) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `budget_owner_id` int(11) DEFAULT NULL,
+  `budget_permission` int(1) DEFAULT 0,
+  PRIMARY KEY (`budget_id`),
+  KEY `budget_parent_id` (`budget_parent_id`),
+  KEY `budget_code` (`budget_code`),
+  KEY `budget_branchcode` (`budget_branchcode`),
+  KEY `budget_period_id` (`budget_period_id`),
+  KEY `budget_owner_id` (`budget_owner_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
