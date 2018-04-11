6de310b91bf18a0ff87a51475a55efafc970ad9d
Simon Hohl <simon.hohl@dainst.org>
2018-04-11 12:25:21 +0200

    Add budget scripts to main export script

--- oracle_exports.py
+++ oracle_exports.py
@@ -1,6 +1,6 @@
 import logging
 import sys
-from lib import aqbooksellers, aqcontacts, aqbasketgroups, aqbasket
+from lib import aqbooksellers, aqcontacts, aqbasketgroups, aqbasket, aqbudgetperiods, aqbudgets
 
 logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -13,7 +13,9 @@ if __name__ == '__main__':
         logger.error('1) Connection info and credentials, pattern: "%USER%/%PASSWORD%@%IP%/%SID%".')
         sys.exit()
 
+    aqbudgetperiods.start(sys.argv[1])
     aqbooksellers.start(sys.argv[1])
+    aqbudgets.start(sys.argv[1])
     aqcontacts.start(sys.argv[1])
     aqbasketgroups.start(sys.argv[1])
     aqbasket.start(sys.argv[1])
