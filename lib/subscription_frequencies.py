0cc15cfce68d68b83a2060c9b39a93173679e6ef
Simon Hohl <simon.hohl@dainst.org>
2018-05-15 12:17:06 +0200

    Add script for extracting subscription frequencies
    Closes Redmine Ticket #7767.

---
+++ lib/subscription_frequencies.py
@@ -0,0 +1,132 @@
+import logging
+import sys
+import os
+import re
+
+import lib.database_connections.oracle as oracle
+import lib.database_connections.mariadb as mariadb
+import lib.mappings.library_keys as library_keys
+import lib.oracle_helper.dates as dates_helper
+
+logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+'''
+`id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `displayorder` int(11) DEFAULT NULL,
+  `unit` enum('day','week','month','year') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `unitsperissue` int(11) NOT NULL DEFAULT '1',
+  `issuesperunit` int(11) NOT NULL DEFAULT '1',
+  '''
+
+UNIT_MAPPING = {
+    'Y': 'year',
+    'M': 'month',
+    'W': 'week',
+    'D': 'day'
+}
+
+FREQUENCY_COUNTER = {}
+
+DESCRIPTION_MAPPING = {
+    'year': 'Jahr',
+    'month': 'Monat',
+    'week': 'Woche',
+    'day': 'Tag'
+}
+
+
+def create_description(unit, units_per_issue):
+    global DESCRIPTION_MAPPING
+
+    if units_per_issue == 1:
+        if unit == 'year':
+            return 'jährlich'
+        elif unit == 'month':
+            return 'monatlich'
+        elif unit == 'week':
+            return 'wöchentlich'
+        elif unit == 'day':
+            return 'täglich'
+    else:
+        if unit == 'year':
+            return 'alle %i Jahre' % units_per_issue
+        elif unit == 'month':
+            return 'alle %i Monate' % units_per_issue
+        elif unit == 'week':
+            return 'alle %i Wochen' % units_per_issue
+        elif unit == 'day':
+            return 'alle %i Tage' % units_per_issue
+
+
+def parse_frequencies(result_dict, data_row):
+
+    global UNIT_MAPPING
+    global FREQUENCY_COUNTER
+
+    unit = UNIT_MAPPING[data_row[14].strip()]
+    units_per_issue = data_row[13]
+    if units_per_issue == 0:
+        unit = UNIT_MAPPING[data_row[10].strip()]
+        units_per_issue = data_row[9]
+
+    if units_per_issue == 0:
+        logger.debug(data_row)
+
+    description = create_description(unit, units_per_issue)
+
+    result = {
+        'description': description,
+        'unit': unit,
+        'unitsperissue': units_per_issue,
+        'issuesperunit': 1
+    }
+
+    key = (unit, units_per_issue)
+
+    if key not in result_dict:
+        result_dict[key] = result
+
+    if key not in FREQUENCY_COUNTER:
+        FREQUENCY_COUNTER[key] = 1
+    else:
+        FREQUENCY_COUNTER[key] += 1
+
+    return result_dict
+
+
+def fetch_data(credentials):
+
+    oracle.establish_connection(credentials)
+
+    cursor = oracle.get_z08_data()
+    frequencies = dict()
+    for row in cursor:
+        frequencies = parse_frequencies(frequencies, row)
+    cursor.close()
+
+    sorted_frequency_counter = sorted(FREQUENCY_COUNTER, key=FREQUENCY_COUNTER.get)
+    display_order = len(sorted_frequency_counter)
+    for idx in sorted_frequency_counter:
+        # logger.debug('%i -- %s' %(FREQUENCY_COUNTER[idx], frequencies[idx]))
+        frequencies[idx]['displayorder'] = display_order
+        display_order -= 1
+
+    return frequencies
+
+
+def start(credentials):
+    frequencies = fetch_data(credentials)
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        logger.info('Please provide as argument:')
+        logger.info('1) Connection info and credentials, pattern: "%USER%/%PASSWORD%@%IP%/%SID%".')
+        sys.exit()
+
+    start(sys.argv[1])
