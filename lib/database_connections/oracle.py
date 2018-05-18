c9f87d94d448e632e9dc1e44181d7caa00096e29
Simon Hohl <simon.hohl@dainst.org>
2018-05-18 17:15:44 +0200

    Add query for subscription data

--- lib/database_connections/oracle.py
+++ lib/database_connections/oracle.py
@@ -169,3 +169,13 @@ def get_z08_data():
     global connection
     cur = connection.cursor()
     return cur.execute('SELECT * FROM Z08')
+
+
+def get_subscription_data():
+    global connection
+    cur = connection.cursor()
+    return cur.execute('SELECT Z16.*, Z601.Z601_REC_KEY, Z68.Z68_REC_KEY FROM Z16, Z601, Z68 ' +
+                       'WHERE ' +
+                       'Z16.Z16_ORDER_NUMBER = Z68.Z68_ORDER_NUMBER ' +
+                       'AND Z68.Z68_REC_KEY = Z601.Z601_REC_KEY_3 ' +
+                       'AND Z601.Z601_TYPE=:1', ('ENC',))
