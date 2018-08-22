8e9adb49a49ba3407b58a93ec4250a679cb61975
Sven Wolter <sven.wolter@dainst.de>
2018-08-22 17:48:17 +0200

    #9259 - Revise and refactor thesaurus mapping on fields 600, 650, and 651

--- lib/marc_bibliographic/thesaurus.py
+++ lib/marc_bibliographic/thesaurus.py
@@ -2,14 +2,14 @@ import logging
 import re
 
 from pymarc import Field, Record
-from typing import List
+from typing import List, Tuple
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 THESAURUS_FIELD_CODE: str = '999'
-NOTATION_600_LIST: List[str] = [
+NOTATION_600_TUPLE: Tuple[str] = (
     'xArchitKgArchitektEinz',
     'xBthChrBen',
     'xBthGMythBen',
@@ -66,8 +66,8 @@ NOTATION_600_LIST: List[str] = [
     'zFests.+',
     '3\.00\.03\.02',
     '3\.01\.04'
-]
-NOTATION_600_MYTH_SUB_LIST: List[str] = [
+)
+NOTATION_600_MYTH_SUB_TUPLE: Tuple[str] = (
     'xBthGMythBen',
     'xPlastRundplGruppGMyth',
     'xPlastRundplKopfGMyth',
@@ -80,8 +80,8 @@ NOTATION_600_MYTH_SUB_LIST: List[str] = [
     'yRGKArch10 - GeselReligGötte.+',
     'yRGKArch11 - GeselReligKulteGötte.+',
     'yRGKArch12 - GeselReligKulteGötte.+'
-]
-NOTATION_651_INCLUDE_LIST: List[str] = [
+)
+NOTATION_651_INCLUDE_TUPLE: Tuple[str] = (
     'xAntWirtschOrt',
     'xArchitBauKirch.+',
     'xArchitKgLandsch',
@@ -163,8 +163,8 @@ NOTATION_651_INCLUDE_LIST: List[str] = [
     '3\.00\.01\.02\..+',
     '4\.02',
     '4\.02\..+'
-]
-NOTATION_651_EXCLUDE_LIST: List[str] = [
+)
+NOTATION_651_EXCLUDE_TUPLE: Tuple[str] = (
     'xEpigrGrEWGAllg',
     'xEpigrGrEWGMehr',
     'xEpigrLatEWGAllg',
@@ -179,170 +179,95 @@ NOTATION_651_EXCLUDE_LIST: List[str] = [
     'xTopWegAllg',
     'xVgImport',
     'xVgUmf'
-]
+)
 MYTH_NOTATION_SUBFIELD_C: str = "(Mythological character)"
 
 
-def map_thesaurus_on_marc_600(marc_thesaurus_field: Field) -> Field:
+def map_thesaurus_on_marc_6xx(marc_thesaurus_field, mapping_field_nr: str) -> Field:
+    is_600_myth_notation_match = False
     thesaurus_subfield_a: str = marc_thesaurus_field['a']
     thesaurus_subfield_e: str = marc_thesaurus_field['e']
     thesaurus_subfield_m: str = marc_thesaurus_field['m']
     thesaurus_subfield_r: str = marc_thesaurus_field['r']
     thesaurus_subfield_1: str = marc_thesaurus_field['1']
+    field_6xx = Field(tag=mapping_field_nr, indicators=['\\', '\\'])
+
+    if mapping_field_nr == '600':
+        for notation_600_myth_reg_exp in NOTATION_600_MYTH_SUB_TUPLE:
+            notation_600_myth_match = re.match(notation_600_myth_reg_exp, thesaurus_subfield_1)
+
+            if notation_600_myth_match is not None:
+                logger.debug("600_myth_match: %s (reg_exp='%s')",
+                             str(notation_600_myth_match), notation_600_myth_reg_exp)
+                is_600_myth_notation_match = True
+            break
+
+    if \
+            thesaurus_subfield_e is not None or \
+            thesaurus_subfield_m is not None or \
+            thesaurus_subfield_r is not None:
+
+        if thesaurus_subfield_e is not None:
+            field_6xx.add_subfield('a', thesaurus_subfield_e)
+
+        if thesaurus_subfield_m is not None:
+            field_6xx.add_subfield('a', thesaurus_subfield_m)
+
+        if thesaurus_subfield_r is not None:
+            field_6xx.add_subfield('a', thesaurus_subfield_r)
+
+        if is_600_myth_notation_match:
+            field_6xx.add_subfield('c', MYTH_NOTATION_SUBFIELD_C)
+
+        field_6xx.add_subfield('g', thesaurus_subfield_a)
+
+    else:
+        field_6xx.add_subfield('a', thesaurus_subfield_a)
+
+        if is_600_myth_notation_match:
+            field_6xx.add_subfield('c', MYTH_NOTATION_SUBFIELD_C)
+
+    field_6xx.add_subfield('2', thesaurus_subfield_1)
+
+    return field_6xx
+
+
+def identify_mapping_field_nr(thesaurus_subfield_1: str) -> str:
     is_600_notation_match: bool = False
-    is_600_myth_notation_match: bool = False
-    field_600: Field = None
-
-    if thesaurus_subfield_a is not None and thesaurus_subfield_1 is not None:
-        logger.debug("999$1 = " + thesaurus_subfield_1)
-
-        for notation_600_reg_exp in NOTATION_600_LIST:
-            notation_600_match = re.match(notation_600_reg_exp, thesaurus_subfield_1)
-
-            if notation_600_match is not None:
-                logger.debug("notation_600_match = " + str(notation_600_match))
-                is_600_notation_match = True
-
-                for notation_600_myth_reg_exp in NOTATION_600_MYTH_SUB_LIST:
-                    notation_600_myth_match = re.match(notation_600_myth_reg_exp, thesaurus_subfield_1)
-
-                    if notation_600_myth_match is not None:
-                        logger.debug("notation_600_myth_match = " + str(notation_600_myth_match))
-                        is_600_myth_notation_match = True
-                        break
-                break
-
-        if is_600_notation_match:
-            field_600 = Field(tag='600', indicators=['\\', '\\'])
-
-            if thesaurus_subfield_e is not None or \
-                    thesaurus_subfield_m is not None or \
-                    thesaurus_subfield_r is not None:
-
-                if thesaurus_subfield_e is not None:
-                    field_600.add_subfield('a', thesaurus_subfield_e)
-
-                if thesaurus_subfield_m is not None:
-                    field_600.add_subfield('a', thesaurus_subfield_m)
-
-                if thesaurus_subfield_r is not None:
-                    field_600.add_subfield('a', thesaurus_subfield_r)
-
-                if is_600_myth_notation_match:
-                    field_600.add_subfield('c', MYTH_NOTATION_SUBFIELD_C)
-
-                field_600.add_subfield('g', thesaurus_subfield_a)
-
-            else:
-                field_600.add_subfield('a', thesaurus_subfield_a)
-
-                if is_600_myth_notation_match:
-                    field_600.add_subfield('c', MYTH_NOTATION_SUBFIELD_C)
-
-            field_600.add_subfield('2', thesaurus_subfield_1)
-
-    return field_600
-
-
-def map_thesaurus_on_marc_650(marc_thesaurus_field: Field) -> Field:
-    thesaurus_subfield_a: str = marc_thesaurus_field['a']
-    thesaurus_subfield_e: str = marc_thesaurus_field['e']
-    thesaurus_subfield_m: str = marc_thesaurus_field['m']
-    thesaurus_subfield_r: str = marc_thesaurus_field['r']
-    thesaurus_subfield_1: str = marc_thesaurus_field['1']
     is_651_notation_match: bool = False
-    field_650: Field = None
 
-    if thesaurus_subfield_a is not None and thesaurus_subfield_1 is not None:
-        for incl_651_reg_exp in NOTATION_651_INCLUDE_LIST:
-            incl_651_match = re.match(incl_651_reg_exp, thesaurus_subfield_1)
+    for notation_600_reg_exp in NOTATION_600_TUPLE:
+        notation_600_match = re.match(notation_600_reg_exp, thesaurus_subfield_1)
 
-            if incl_651_match is not None:
-                is_651_notation_match = True
+        if notation_600_match is not None:
+            logger.debug("600_match: %s (reg_exp='%s')", str(notation_600_match), notation_600_reg_exp)
+            is_600_notation_match = True
+            break
 
-                for excl_651_reg_exp in NOTATION_651_EXCLUDE_LIST:
-                    excl_651_match = re.match(excl_651_reg_exp, thesaurus_subfield_1)
+    for incl_651_reg_exp in NOTATION_651_INCLUDE_TUPLE:
+        incl_651_match = re.match(incl_651_reg_exp, thesaurus_subfield_1)
 
-                    if excl_651_match is not None:
-                        is_651_notation_match = False
-                        break
-                break
+        if incl_651_match is not None:
+            logger.debug("651_incl_match: %s (reg_exp='%s')", str(incl_651_match), incl_651_reg_exp)
+            is_651_notation_match = True
 
-        if not is_651_notation_match:
-            field_650 = Field(tag='650', indicators=['\\', '\\'])
+            for excl_651_reg_exp in NOTATION_651_EXCLUDE_TUPLE:
+                excl_651_match = re.match(excl_651_reg_exp, thesaurus_subfield_1)
 
-            if thesaurus_subfield_e is not None or \
-                    thesaurus_subfield_m is not None or \
-                    thesaurus_subfield_r is not None:
+                if excl_651_match is not None:
+                    logger.debug("651_excl_match: %s (reg_exp='%s')", str(excl_651_match), excl_651_reg_exp)
+                    is_651_notation_match = False
+                    break
+            break
 
-                if thesaurus_subfield_e is not None:
-                    field_650.add_subfield('a', thesaurus_subfield_e)
-
-                if thesaurus_subfield_m is not None:
-                    field_650.add_subfield('a', thesaurus_subfield_m)
-
-                if thesaurus_subfield_r is not None:
-                    field_650.add_subfield('a', thesaurus_subfield_r)
-
-                field_650.add_subfield('g', thesaurus_subfield_a)
-
-            else:
-                field_650.add_subfield('a', thesaurus_subfield_a)
-
-            field_650.add_subfield('2', thesaurus_subfield_1)
-
-    return field_650
-
-
-def map_thesaurus_on_marc_651(marc_thesaurus_field: Field) -> Field:
-    thesaurus_subfield_a: str = marc_thesaurus_field['a']
-    thesaurus_subfield_e: str = marc_thesaurus_field['e']
-    thesaurus_subfield_m: str = marc_thesaurus_field['m']
-    thesaurus_subfield_r: str = marc_thesaurus_field['r']
-    thesaurus_subfield_1: str = marc_thesaurus_field['1']
-    is_651_notation_match: bool = False
-    field_651: Field = None
-
-    if thesaurus_subfield_a is not None and thesaurus_subfield_1 is not None:
-        logger.debug("999$1 = " + thesaurus_subfield_1)
-
-        for incl_651_reg_exp in NOTATION_651_INCLUDE_LIST:
-            incl_651_match = re.match(incl_651_reg_exp, thesaurus_subfield_1)
-
-            if incl_651_match is not None:
-                logger.debug("incl_651_match = " + str(incl_651_match))
-                is_651_notation_match = True
-
-                for excl_651_reg_exp in NOTATION_651_EXCLUDE_LIST:
-                    excl_651_match = re.match(excl_651_reg_exp, thesaurus_subfield_1)
-
-                    if excl_651_match is not None:
-                        logger.debug("excl_651_match = " + str(excl_651_match))
-                        is_651_notation_match = False
-                        break
-                break
-
-        if is_651_notation_match:
-            field_651 = Field(tag='651', indicators=['\\', '\\'])
-
-            if thesaurus_subfield_e is not None or thesaurus_subfield_m is not None or thesaurus_subfield_r is not None:
-                if thesaurus_subfield_e is not None:
-                    field_651.add_subfield('a', thesaurus_subfield_e)
-
-                if thesaurus_subfield_m is not None:
-                    field_651.add_subfield('a', thesaurus_subfield_m)
-
-                if thesaurus_subfield_r is not None:
-                    field_651.add_subfield('a', thesaurus_subfield_r)
-
-                field_651.add_subfield('g', thesaurus_subfield_a)
-
-            else:
-                field_651.add_subfield('a', thesaurus_subfield_a)
-
-            field_651.add_subfield('2', thesaurus_subfield_1)
-
-    return field_651
+    if is_600_notation_match and is_651_notation_match:
+        raise ValueError
+    elif is_600_notation_match:
+        return '600'
+    elif is_651_notation_match:
+        return '651'
+    else:
+        return '650'
 
 
 def map_thesaurus(record: Record) -> int:
@@ -355,22 +280,21 @@ def map_thesaurus(record: Record) -> int:
     thesaurus_field_counter: int = 1
     for marc_thesaurus_field in marc_thesaurus_fields:
         logger.debug("Field No. %s: %s", thesaurus_field_counter, marc_thesaurus_field)
-        field_650: Field = map_thesaurus_on_marc_650(marc_thesaurus_field)
-        field_651: Field = map_thesaurus_on_marc_651(marc_thesaurus_field)
+        thesaurus_subfield_a: str = marc_thesaurus_field['a']
+        thesaurus_subfield_1: str = marc_thesaurus_field['1']
 
-        if field_650 is not None and field_651 is not None:
-            logger.error("Field No. %s: No unique mapping!\nThesaurus field %s is mapped on:\n%s and\n%s)",
-                         thesaurus_field_counter, marc_thesaurus_field, field_650, field_651)
-            is_record_format_error = True
-            record_error_no += 1
-        else:
-            if field_650 is not None:
-                record.add_field(field_650)
-                logger.debug("Field No. %s: %s", thesaurus_field_counter, field_650)
-
-            if field_651 is not None:
-                record.add_field(field_651)
-                logger.debug("Field No. %s: %s", thesaurus_field_counter, field_651)
+        if thesaurus_subfield_a is not None and thesaurus_subfield_1 is not None:
+            try:
+                field_nr: str = identify_mapping_field_nr(thesaurus_subfield_1)
+                field_6xx: Field = map_thesaurus_on_marc_6xx(marc_thesaurus_field, field_nr)
+                record.add_field(field_6xx)
+                logger.debug("Field No. %s: %s", thesaurus_field_counter, field_6xx)
+            except ValueError:
+                logger.error("Field No. %s: No unique mapping for notation %s\n"
+                             "(Thesaurus is mapped on field 600 and 651)",
+                             thesaurus_field_counter, thesaurus_subfield_1)
+                is_record_format_error = True
+                record_error_no += 1
 
         thesaurus_field_counter += 1
 
