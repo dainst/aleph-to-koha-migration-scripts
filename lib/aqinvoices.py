f27b90f57a849f6d65e9d3fb5c9cc5a54e7af7d5
Simon Hohl <simon.hohl@dainst.org>
2018-08-30 14:58:21 +0200

    Add missing forward slash in path

--- lib/aqinvoices.py
+++ lib/aqinvoices.py
@@ -77,7 +77,7 @@ def fetch_data(credentials):
 
     logger.debug(' %i of %i orders are missing a budget.' % (len(MISSING_BUDGET), counter))
 
-    with open(script_dir + '../log/missing_budget_aqinvoices.log', 'w') as log_file:
+    with open(script_dir + '/../log/missing_budget_aqinvoices.log', 'w') as log_file:
         for order in MISSING_BUDGET:
             log_file.write('%s\n' % order)
 
