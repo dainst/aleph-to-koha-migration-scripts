1414422dda7b3faf257c8a848c7ea19d7a243bb1
Simon Hohl <simon.hohl@dainst.org>
2020-07-16 14:10:41 +0200

    Add vendor code to contact notes field

--- lib/aqcontacts.py
+++ lib/aqcontacts.py
@@ -86,6 +86,10 @@ def process_z72_result(previous_results, query_result):
             aqcontacts_data['notes'] = ''
         aqcontacts_data['notes'] += ', ' + parsed_z70_data['notes']
 
+    if aqcontacts_data['notes'] is None:
+        aqcontacts_data['notes'] = ''
+    aqcontacts_data['notes'] += '\nHinweis: ' + str(query_result[0][:-2]).rstrip()
+
     if all(value is None for value in aqcontacts_data.values()):
         return previous_results
 
