97ec9c818ad62254a9f14adb98d8563d613a9af5
Simon Hohl <simon.hohl@dainst.org>
2020-08-10 13:27:56 +0200

    Provide missing helper function

--- lib/oracle_helper/z68.py
+++ lib/oracle_helper/z68.py
@@ -8,7 +8,13 @@ logger.setLevel(logging.WARNING)
 def evaluate_is_standing(aleph_order_type):
     aleph_order_type = aleph_order_type.strip()
 
-    if aleph_order_type == 'O':
+    if aleph_order_type == 'O' or aleph_order_type == 'S':
         return 1
 
     return 0
+
+
+def evaluate_is_subscription(aleph_order_type):
+    aleph_order_type = aleph_order_type.strip()
+
+    return aleph_order_type == 'S'
